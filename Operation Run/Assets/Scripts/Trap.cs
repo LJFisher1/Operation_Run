6d9ad38ad26ccc29829105c79838f80998ed0109
LJFisher1 <ljfisher1@student.fullsail.edu>
2023-03-20 16:19:24 -0500

    Trap Prefab

--- Operation Run/Assets/Scripts/Trap.cs
+++ Operation Run/Assets/Scripts/Trap.cs
@@ -7,18 +7,21 @@ public class Trap : MonoBehaviour
 {
     [SerializeField] int timer;
     [SerializeField] GameObject explosion;
+    [SerializeField] int damage;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             StartCoroutine(Expl());
+           
         }
     }
     IEnumerator Expl()
     {
         yield return new WaitForSeconds(timer);
         Instantiate(explosion, transform.position, explosion.transform.rotation);
+        GameManager.instance.playerController.TakeDamage(damage);
         Destroy(gameObject);
     }
 }
