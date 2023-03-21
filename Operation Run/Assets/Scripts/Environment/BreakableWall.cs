cacc783b4535544c45d5ac636fb7bd023321a14b
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-03-21 18:50:01 -0500

    Updated Mana Pickup Prefab Visuals

--- Operation Run/Assets/Scripts/Environment/BreakableWall.cs
+++ Operation Run/Assets/Scripts/Environment/BreakableWall.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class BreakableWall : MonoBehaviour, IDamage
 {
-    //public void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        if (GameManager.instance.playerController.isUsingWeapon == true && GameManager.instance.playerController.weapon.canBreakWalls == true)
-    //        {
-    //            Destroy(gameObject);
-    //        }
-    //    }
-    //}
-
     public void TakeDamage(int dmg)
     {
         if (GameManager.instance.playerController.weapon.canBreakWalls == true)
