60d0a07cb85d59d04ab6d19f510e18b870bcc080
Gary <GkGroves@student.fullsail.edu>
2023-03-21 00:21:06 -0400

    Door Movement

--- Operation Run/Assets/Scripts/KeysAndDoors/DoorOpen.cs
+++ Operation Run/Assets/Scripts/KeysAndDoors/DoorOpen.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DoorOpen : MonoBehaviour
 {
+    [Header("---- Settings ----")]
+    [SerializeField] float doorSpeed;
+    public Transform movePosition;
+    Vector3 moveVector;
+    bool moving;
+
+    [Header("---- Audio ----")]
     [SerializeField] AudioClip[] doorOpen;
     [Range(0, 1)] [SerializeField] float volumeAudio;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && GameManager.instance.playerController.keysInPossession >= 1)
@@ -13,12 +22,36 @@ public class DoorOpen : MonoBehaviour
             GameManager.instance.playerController.PlayAud(doorOpen, volumeAudio);
             GameManager.instance.playerController.KeyUsed();
             GameManager.instance.usedKey1();
-            Destroy(gameObject);
             GameManager.instance.UpdateScore(5);
+            moving = true;
         }
         else if (other.CompareTag("Player") && GameManager.instance.playerController.keysInPossession <= 0)
         {
             StartCoroutine(GameManager.instance.noKeysFlash());
         }
+    }
+
+    private void Start()
+    {
+        moveVector = movePosition.position;
+    }
+
+    private void Update()
+    {
+        if(moving)
+        {
+            slideTheDoor();
+        }
+    }
+
+    void slideTheDoor()
+    {
+        float distance = Vector3.Distance(transform.position, moveVector);
+        Debug.Log(distance);
+        if (distance > 0.5f)
+        {
+            transform.position = Vector3.Lerp(transform.position, moveVector, Time.deltaTime * doorSpeed);
+        }
+        else { Destroy(gameObject); }
     }
 }
