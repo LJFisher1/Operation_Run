61be9f44bceb2d06822348410fb61ce119d97c42
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-04-01 22:46:05 -0500

    Adjusted Melee weapon

--- Operation Run/Assets/Scripts/Weapons/MeleeMagic.cs
+++ Operation Run/Assets/Scripts/Weapons/MeleeMagic.cs
@@ -13,22 +13,31 @@ public class MeleeMagic : MonoBehaviour, IBullet
     [SerializeField] float verticalInfluenceMult;
     [SerializeField] public Vector3 bouncBackDirInfluence;
     [SerializeField] Collider dashCollider;
+    [SerializeField] GameObject shield;
+    GameObject shieldClone;
     [SerializeField] GameObject hitEffect;
+    PlayerController player;
     Vector3 dashDir;
     public void Initialize(Weapon creator)
     {
         damage = creator.damage;
         duration = creator.duration;
         dashSpeed = creator.bulletSpeed;
+        player = GameManager.instance.playerController;
         StartCoroutine(Charge());
     }
     private void Update()
     {
-        transform.position = GameManager.instance.playerController.shootPointVisual.position;
-        transform.rotation = GameManager.instance.playerController.transform.rotation;
-        if (!GameManager.instance.playerController.IsAlive)
+        transform.position = player.shootPointVisual.position;
+        transform.rotation = player.transform.rotation;
+        if (shieldClone)
         {
-            Destroy(gameObject);
+            shieldClone.transform.position = transform.position;
+            shieldClone.transform.rotation = transform.rotation;
+        }
+        if (!player.IsAlive)
+        {
+            EndDash();
         }
     }
     public void OnTriggerEnter(Collider other)
@@ -46,7 +55,7 @@ public class MeleeMagic : MonoBehaviour, IBullet
                     dam.TakeDamage(damage);
                 }
             }
-            GameManager.instance.playerController.ApplyForce((-dashDir + bouncBackDirInfluence) * dashSpeed * bounceBackMultiplier);
+            player.ApplyForce(bounceBackMultiplier * dashSpeed * (-dashDir + bouncBackDirInfluence));
             Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
             EndDash();
         }
@@ -62,9 +71,10 @@ public class MeleeMagic : MonoBehaviour, IBullet
         dashing = true;
         dashDir = Camera.main.transform.forward;
         dashDir = new(dashDir.x, dashDir.y * verticalInfluenceMult, dashDir.z);
+        shieldClone = Instantiate(shield,transform.position,transform.rotation);
         dashCollider.GetComponent<MeshRenderer>().enabled = true;
         Camera.main.GetComponent<CameraController>().enabled = false;
-        GameManager.instance.playerController.ApplyForce(dashDir * dashSpeed);
+        player.ApplyForce(dashDir * dashSpeed);
         yield return new WaitForSeconds(duration/2);
         dashing = false;
         EndDash();
@@ -73,6 +83,7 @@ public class MeleeMagic : MonoBehaviour, IBullet
     void EndDash()
     {
         Camera.main.GetComponent<CameraController>().enabled = true;
+        Destroy(shieldClone, duration/5);
         Destroy(gameObject);
     }
 }
