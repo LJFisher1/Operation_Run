f6de6cafb82901ccd6df4fb222539bb06a541ce2
Gary <GkGroves@student.fullsail.edu>
2023-04-05 10:11:03 -0400

    Added SwingDoor
    * Swings open
    * Can be locked

---
+++ Operation Run/Assets/swingDoor.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class swingDoor : MonoBehaviour
+{
+    [Header("* Controls")]
+    [SerializeField] bool interaction;
+    [SerializeField] bool doorLocked;
+    [SerializeField] bool doorOpen;
+    [Header("* Settings")]
+    [SerializeField] float speed;
+    [SerializeField] Quaternion rotationOpenL;
+    [SerializeField] Quaternion rotationOpenR;
+    [SerializeField] Quaternion rotationClose;
+    [Header("* Components")]
+    [SerializeField] Transform doorLeft;
+    [SerializeField] Transform doorRight;
+    [SerializeField] GameObject padlock;
+    [SerializeField] AudioClip[] audioUnlock;
+    [SerializeField] AudioClip[] audioOpenDoor;
+    [SerializeField] AudioClip[] audioCloseDoor;
+    [Range(0, 1)][SerializeField] float volume;
+
+    private void Awake()
+    {
+        doorOpen = false;
+        interaction = false;
+        if(doorLocked)
+        {
+            padlock.SetActive(true);
+        }
+        else
+        {
+            padlock.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") & doorOpen == false)
+        {
+            if (doorLocked && GameManager.instance.playerController.keysInPossession > 0)
+            {
+                StartCoroutine(UnlockDoor());
+            }
+            else if (doorLocked == false)
+            {
+                doorOpen = true;
+                interaction = true;
+            }
+            
+        }
+    }
+
+    private void Update()
+    {
+        if(interaction)
+        {
+            MoveDoor();
+            StartCoroutine(TurnOffDoor());
+        }
+    }
+
+    IEnumerator UnlockDoor()
+    {
+        GameManager.instance.playerController.KeyUsed();
+        doorLocked = false;
+        padlock.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        interaction = true;
+        doorOpen = true;
+    }
+
+    void MoveDoor()
+    {
+        if (doorOpen)
+        {
+            doorLeft.rotation = Quaternion.Lerp(doorLeft.rotation, rotationOpenL, Time.deltaTime * speed);
+            doorRight.rotation = Quaternion.Lerp(doorRight.rotation, rotationOpenR, Time.deltaTime * speed);
+        }
+        else
+        {
+            doorLeft.rotation = Quaternion.Lerp(doorLeft.rotation, rotationClose, Time.deltaTime * speed);
+            doorRight.rotation = Quaternion.Lerp(doorRight.rotation, rotationClose, Time.deltaTime * speed);
+        }
+    }
+
+    IEnumerator TurnOffDoor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        interaction = false;
+    }
+}
