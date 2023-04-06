76813b15a32a7e6528f853eeb9fecf377fd83099
nlogann <nlogann115@gmail.com>
2023-04-06 17:14:22 -0500

    Fixed null reference
    added an else to onDestroy for if there is no player in the scene

--- Operation Run/Assets/Scripts/Weapons/GiveWeaponOnDestroy.cs
+++ Operation Run/Assets/Scripts/Weapons/GiveWeaponOnDestroy.cs
@@ -10,6 +10,7 @@ public class GiveWeaponOnDestroy : MonoBehaviour
     {
         //have to check if player exists or bizzarly, you get a bunch of errors when you stop the project. I assume this has something to do with the destory order.
         //In general doing this on destroy probably isnt the best but it works well for now & is super easy to put on stuff.
-        if(GameManager.instance.player != null) GameManager.instance.playerController.ChangeWeapon(weapon);
+        if (GameManager.instance.player != null) GameManager.instance.playerController.ChangeWeapon(weapon);
+        else return;
     }
 }
