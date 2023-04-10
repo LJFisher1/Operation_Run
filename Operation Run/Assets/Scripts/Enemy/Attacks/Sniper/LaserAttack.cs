327b33826104b7953ab840dc5d1c1f18d84d72b0
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-04-09 21:34:26 -0500

    Fixed Sniper tracer lingering after death

--- Operation Run/Assets/Scripts/Enemy/Attacks/Sniper/LaserAttack.cs
+++ Operation Run/Assets/Scripts/Enemy/Attacks/Sniper/LaserAttack.cs
@@ -28,6 +28,7 @@ public class LaserAttack : MonoBehaviour, IEnemyAttack
     bool telegraphing;
     bool hasFired;
     EnemyAI enemy;
+    bool HasEnemy { get => (enemy == null || !enemy.IsAlive); }
 
     public void Initialize(EnemyAI creator)
     {
@@ -41,76 +42,92 @@ public class LaserAttack : MonoBehaviour, IEnemyAttack
     }
     private void Update()
     {
-        if (takingAim || telegraphing)
+        if (!HasEnemy)
         {
-            lineRend.SetPosition(0, enemy.projectilePosition.position);
-            Vector3 endpoint = GameManager.instance.player.transform.position;
-            WallCheck(ref endpoint);
-            lineRend.SetPosition(1, Vector3.Lerp(lineRend.GetPosition(1), endpoint, followSpeed * Time.deltaTime));
-            
-            //lineRend.endColor = Color.Lerp(Color.clear, Color.blue, lineDecaySpeed*10 * Time.deltaTime);
-            //lineRend.startColor = Color.Lerp(Color.clear, Color.blue, lineDecaySpeed*10 * Time.deltaTime);
-            //lineRend.endWidth = Mathf.Lerp(0, 5, lineDecaySpeed * Time.deltaTime);
-            //lineRend.startWidth = Mathf.Lerp(0, 3, lineDecaySpeed * Time.deltaTime);
-        }
-        if (hasFired)
-        {
-            lineRend.endColor = Color.Lerp(lineRend.endColor, Color.clear, lineDecaySpeed * Time.deltaTime);
-            lineRend.startColor = Color.Lerp(lineRend.startColor, Color.clear, lineDecaySpeed * Time.deltaTime);
-            lineRend.endWidth = Mathf.Lerp(lineRend.endWidth, 0, lineDecaySpeed * Time.deltaTime);
-            lineRend.startWidth = Mathf.Lerp(lineRend.startWidth, 0, lineDecaySpeed * Time.deltaTime);
-        }
+            if (takingAim || telegraphing)
+            {
+                lineRend.SetPosition(0, enemy.projectilePosition.position);
+                Vector3 endpoint = GameManager.instance.player.transform.position;
+                WallCheck(ref endpoint);
+                lineRend.SetPosition(1, Vector3.Lerp(lineRend.GetPosition(1), endpoint, followSpeed * Time.deltaTime));
 
+                //lineRend.endColor = Color.Lerp(Color.clear, Color.blue, lineDecaySpeed*10 * Time.deltaTime);
+                //lineRend.startColor = Color.Lerp(Color.clear, Color.blue, lineDecaySpeed*10 * Time.deltaTime);
+                //lineRend.endWidth = Mathf.Lerp(0, 5, lineDecaySpeed * Time.deltaTime);
+                //lineRend.startWidth = Mathf.Lerp(0, 3, lineDecaySpeed * Time.deltaTime);
+            }
+            if (hasFired)
+            {
+                lineRend.endColor = Color.Lerp(lineRend.endColor, Color.clear, lineDecaySpeed * Time.deltaTime);
+                lineRend.startColor = Color.Lerp(lineRend.startColor, Color.clear, lineDecaySpeed * Time.deltaTime);
+                lineRend.endWidth = Mathf.Lerp(lineRend.endWidth, 0, lineDecaySpeed * Time.deltaTime);
+                lineRend.startWidth = Mathf.Lerp(lineRend.startWidth, 0, lineDecaySpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            this.StopAllCoroutines();
+            Destroy(gameObject);
+        }
+        
     }
 
     IEnumerator Aim()
     {
-        aud.PlayOneShot(aimSound);
-        takingAim = true;
-        lineRend.colorGradient = aimColor;
-        yield return new WaitForSeconds(aimTime);
-        takingAim = false;
-        StartCoroutine( FireTelegraph());
+        if (!HasEnemy)
+        {
+            aud.PlayOneShot(aimSound);
+            takingAim = true;
+            lineRend.colorGradient = aimColor;
+            yield return new WaitForSeconds(aimTime);
+            takingAim = false;
+        }
+        StartCoroutine(FireTelegraph());
     }
 
     IEnumerator FireTelegraph()
     {
-        telegraphing = true;
-        lineRend.colorGradient = telegraphFlashColor;
-        yield return new WaitForSeconds(telegraphTime / 3);
-        lineRend.colorGradient = aimColor;
-        yield return new WaitForSeconds(telegraphTime / 3);
-        lineRend.colorGradient = telegraphFlashColor;
-        yield return new WaitForSeconds(telegraphTime / 3);
-        telegraphing = false;
+        if (!HasEnemy)
+        {
+            telegraphing = true;
+            lineRend.colorGradient = telegraphFlashColor;
+            yield return new WaitForSeconds(telegraphTime / 3);
+            lineRend.colorGradient = aimColor;
+            yield return new WaitForSeconds(telegraphTime / 3);
+            lineRend.colorGradient = telegraphFlashColor;
+            yield return new WaitForSeconds(telegraphTime / 3);
+            telegraphing = false;
+        }
         StartCoroutine(Fire());
     }
     IEnumerator Fire()
     {
-
-        aud.PlayOneShot(fireSound);
-        Instantiate(hitEffect, enemy.projectilePosition.position, enemy.transform.rotation);
-        enemy.animator.SetTrigger("Shoot");
-        lineRend.colorGradient = fireColor;
-        RaycastHit hit;
-        Vector3 direction = (lineRend.GetPosition(1) - enemy.projectilePosition.position ).normalized;
-        Ray ray = new(enemy.projectilePosition.position, direction);
-        if (Physics.Raycast(ray, out hit, range))
+        if (!HasEnemy)
         {
-            Debug.Log(hit.transform.name);
-            var target = hit.collider.GetComponent<IDamage>();
-            if (target != null && hit.transform.CompareTag("Player"))
+            aud.PlayOneShot(fireSound);
+            Instantiate(hitEffect, enemy.projectilePosition.position, enemy.transform.rotation);
+            enemy.animator.SetTrigger("Shoot");
+            lineRend.colorGradient = fireColor;
+            RaycastHit hit;
+            Vector3 direction = (lineRend.GetPosition(1) - enemy.projectilePosition.position).normalized;
+            Ray ray = new(enemy.projectilePosition.position, direction);
+            if (Physics.Raycast(ray, out hit, range))
             {
-                target.TakeDamage(damage);
-                //if (hit.transform.GetComponent<TeleportBarrel>() != null || !target.IsAlive)
-                //{
-                //    TeleportAbility(hit.transform.position);
-                //}
+                Debug.Log(hit.transform.name);
+                var target = hit.collider.GetComponent<IDamage>();
+                if (target != null && hit.transform.CompareTag("Player"))
+                {
+                    target.TakeDamage(damage);
+                    //if (hit.transform.GetComponent<TeleportBarrel>() != null || !target.IsAlive)
+                    //{
+                    //    TeleportAbility(hit.transform.position);
+                    //}
+                }
+                Instantiate(hitEffect, hit.point, enemy.transform.rotation);
             }
-            Instantiate(hitEffect, hit.point, enemy.transform.rotation);
+            yield return new WaitForSeconds(decayAfterFire);
+            hasFired = true;
         }
-        yield return new WaitForSeconds(decayAfterFire);
-        hasFired = true;
         Destroy(gameObject, duration);
     }
 
