efa8e949115c547210943bdcfcb5bddcf1e8231b
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-04-11 21:29:22 -0500

    fixed ghost staff damaging triggers

--- Operation Run/Assets/Scripts/Weapons/Ghost/GhostBulletDamage.cs
+++ Operation Run/Assets/Scripts/Weapons/Ghost/GhostBulletDamage.cs
@@ -7,6 +7,7 @@ public class GhostBulletDamage : MonoBehaviour
     [SerializeField] GhostBullet gb;
     private void OnTriggerStay(Collider other)
     {
+        if (other.isTrigger) return;
         IDamage damAble = other.GetComponent<IDamage>();
         if(damAble != null)
         {
