2a325db5cbcab2654fb1f36ac0ba508fcc20b68d
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-04-19 18:45:23 -0500

    Bob Script adjustments

--- Operation Run/Assets/Scripts/BobUpAndDown.cs
+++ Operation Run/Assets/Scripts/BobUpAndDown.cs
@@ -9,6 +9,10 @@ public class BobUpAndDown : MonoBehaviour
     [SerializeField] float yOff;
     [SerializeField] bool useParent;
     [SerializeField] float bobSpeed;
+    [SerializeField] float randomOffset;
+    [SerializeField] float randomSpeed;
+    float rOff;
+    float rBob;
     bool movingUp;
     Vector3 posOg;
     void Start()
@@ -22,20 +26,26 @@ public class BobUpAndDown : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
         if (movingUp)
         {
-            body.position = Vector3.Lerp(body.position, Vector3.up * yOff + posOg, bobSpeed * bobSpeed/(body.position - (Vector3.up * yOff + posOg)).magnitude * Time.deltaTime);
-            if((body.position - (Vector3.up * yOff + posOg)).magnitude < .1)
+            body.position = Vector3.Lerp(body.position, Vector3.up * yOff + posOg, (bobSpeed + rBob) * (bobSpeed + rBob) / (body.position - (Vector3.up * (yOff + rOff) + posOg)).magnitude * Time.deltaTime);
+            if((body.position - (Vector3.up * (yOff + rOff) + posOg)).magnitude < .1)
             {
                 movingUp = false;
+                rOff = Random.Range(-randomOffset, randomOffset);
+                rBob = Random.Range(-randomOffset, randomOffset);
             }
         }
         else
         {
-            body.position = Vector3.Lerp(body.position, Vector3.down * yOff + posOg, bobSpeed * bobSpeed/(body.position - (Vector3.down * yOff + posOg)).magnitude * Time.deltaTime);
-            if ((body.position - (Vector3.down * yOff + posOg)).magnitude < .1)
+            body.position = Vector3.Lerp(body.position, Vector3.down * yOff + posOg, (bobSpeed + rBob) * (bobSpeed + rBob) / (body.position - (Vector3.down * (yOff + rOff) + posOg)).magnitude * Time.deltaTime);
+            if ((body.position - (Vector3.down * (yOff + rOff) + posOg)).magnitude < .1)
             {
                 movingUp = true;
+                rOff = Random.Range(-randomOffset, randomOffset);
+                rBob = Random.Range(-randomOffset, randomOffset);
             }
         }
 
