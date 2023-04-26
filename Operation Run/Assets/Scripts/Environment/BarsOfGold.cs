4c7c8a94ff5874c7e183d1a5fd9d4bf35e3ade63
WHHWII <36775534+WHHWII@users.noreply.github.com>
2023-04-26 16:14:30 -0500

    Made large chest on dungeon level give large gold

--- Operation Run/Assets/Scripts/Environment/BarsOfGold.cs
+++ Operation Run/Assets/Scripts/Environment/BarsOfGold.cs
@@ -8,12 +8,14 @@ public class BarsOfGold : MonoBehaviour
     [SerializeField] AudioSource aud;
     [SerializeField] AudioClip[] audGoldPickup;
     [Range(0, 1)] [SerializeField] float audGoldPickupVol;
+    [Header("--- score value ---")]
+    [SerializeField] int scoreValue = 10;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            GameManager.instance.UpdateScore(10, "Gold");
+            GameManager.instance.UpdateScore(scoreValue, "Gold");
             ++GameManager.instance.goldCollected;
             GameManager.instance.playerController.PlayAud(audGoldPickup, audGoldPickupVol);
             Destroy(gameObject);
